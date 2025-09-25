97411fa1a4ef9d687165d5653136c00f2dda2c44
AyushAgrawal468 <AyushAgrawal468@gmail.com>
2025-09-25 23:35:43 +0530

    waitlist form issue fix-1

--- lead-form-frontend/vite.config.js
+++ lead-form-frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://tandem.it.com/api/leads',
         changeOrigin: true,
         secure: false,
       },
