1e94419de4a458c47a3412a501565e0f8279b303
AyushAgrawal468 <AyushAgrawal468@gmail.com>
2025-10-07 11:57:25 +0530

    Oct -07 -2

--- lead-form-frontend/vite.config.js
+++ lead-form-frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://tandem.it.com',
         changeOrigin: true,
         secure: false,
       },
